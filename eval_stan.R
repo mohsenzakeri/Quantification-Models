d03a05b65b960d80a4de8e6ab6f7af99bf9ecc42
mohsenzakeri <mohsen.zakery@gmail.com>
2018-11-05 17:21:43 -0500

    Add plot for the correlation

--- eval_stan.R
+++ eval_stan.R
@@ -1,4 +1,5 @@
 library(rstan)
+library(ggplot2)
 
 stan_fit <- readRDS("large_fit.rds",NULL)
 
@@ -11,3 +12,6 @@ salmon_fit <- read.csv(file="/mnt/scratch2/mohsen/stan-test/salmon-output/quant.
 salmon_ests <- salmon_fit$NumReads
 
 cor.test(salmon_ests, stan_ests, method="spearman")
+
+qplot(log(salmon_ests),log(stan_ests),alpha=0.5)
+ggsave("cor.pdf", plot = last_plot())
