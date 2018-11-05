8a76d32b0ed7454e26fdc0ecca552f48252d3651
mohsenzakeri <mohsen.zakery@gmail.com>
2018-11-05 15:59:02 -0500

    Fix stan model to run quantification

--- run_stan.R
+++ run_stan.R
@@ -1,12 +1,21 @@
 library(rstan)
+require("reticulate")
 
-row_starts <- read.csv("row_starts.csv", header=FALSE, sep=",")
-weights <- read.csv("weights.csv", header=FALSE, sep=",")
-columns <- read.csv("columns.csv", header=FALSE, sep=",")
-counts <- read.csv("parse_eq_classes_file/counts.csv", header=FALSE, sep=",")
+#row_starts <- read.csv("row_starts.csv", header=FALSE, sep=",")
+#weights <- read.csv("weights.csv", header=FALSE, sep=",")
+#columns <- read.csv("columns.csv", header=FALSE, sep=",")
+#counts <- read.csv("parse_eq_classes_file/counts.csv", header=FALSE, sep=",")
 
-stan_data <- list(weights = weights, columns = columns, row_starts = row_starts, counts = counts)
+#stan_data <- list(weights = weights, columns = columns, row_starts = row_starts, counts = counts)
 
-stan_model <- "stan_model_eq_classes.stan"
+source_python("/home/mohsen/pickle_reader.py")
 
-fit <- stan(file = stan_model, data = stan_data, warmup = 500, iter = 1000, chains = 4, cores = 2, thin = 1)
+stan_model <- "stan_model_eq_classes_working.stan"
+stan_data <- read_pickle_file("/home/mohsen/stan_data_small.p")
+
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+fit <- stan(file = stan_model, data = stan_data, warmup = 50, iter = 100, chains = 4, cores = 16, include = TRUE, pars="theta")
+
+saveRDS(fit, file = "/mnt/scratch2/mohsen/stan-test/parse_eq_classes_file/QuantificationModels/small_fit_no_prior_100iter.rds")
--- stan_model_eq_classes.stan
+++ stan_model_eq_classes.stan
@@ -1,29 +1,24 @@
-// Stan model for simple linear regression
-
 data {
- int < lower = 1 > N; // eq classes count
- int < lower = 1 > M; // transcripts count
- int < lower = 1 > Q; // total weighs count
- vector[Q] weights; // eq class conditional probs
- int columns[Q]; // weight vector column indicator
- int row_starts[N+1]; // weight vector row starts
- vector[N] counts; // number of fragments in each eq class
+    int < lower = 1 > N; // eq classes count
+    int < lower = 1 > M; // transcripts count
+    int < lower = 1 > Q; // total weighs count
+    vector[Q] weights; // eq class conditional props
+    int columns[Q]; // weight vector column indicator
+    int row_starts[N+1]; // weight vector row starts
+    vector[N] counts; // number of fragments in each eq class
+    vector[M] alpha;
 }
 
-parameters {
- vector[M] theta; // transcript_counts
+parameters{
+    simplex[M] theta; // transcript_counts
 }
 
 transformed parameters {
- vector[N] likelihood_1;
- vector[N] likelihood;
- likelihood_1 = csr_matrix_times_vector(N, M, weights, columns, row_starts, theta);
- likelihood = likelihood_1 .* counts;
+    vector[N] likelihood; 
+    likelihood = csr_matrix_times_vector(N, M, weights, columns, row_starts, theta) .* counts;
 }
 
-model {
- target += sum(likelihood);
+model{
+    //theta ~ dirichlet(alpha);
+    target += log(likelihood) .* counts;
 }
-
-generated quantities {
-} // The posterior predictive distribution
