ed932062724377495e4f2e5a8071739dcfa20e3e
mohsenzakeri <mohsen.zakery@gmail.com>
2018-11-07 11:11:09 -0500

    Apply effective length in conditional probabilities

--- eval_stan.R
+++ eval_stan.R
@@ -1,15 +1,17 @@
 library(rstan)
 library(ggplot2)
 
-stan_fit <- readRDS("large_fit.rds",NULL)
+stan_fit <- readRDS("small_fit_no_prior_1000iter.rds",NULL)
 
 stan_fit_theta <- summary(stan_fit, pars = c("theta"))$summary
 
 stan_ests <- stan_fit_theta[, c("mean")]
 
-salmon_fit <- read.csv(file="/mnt/scratch2/mohsen/stan-test/salmon-output/quant.sf", header=TRUE, sep="\t")
+salmon_fit <- read.csv(file="/mnt/scratch2/mohsen/stan-test/salmon-0.11.3-linux_x86_64/sample_data/salmon-output/quant.sf", header=TRUE, sep="\t")
 
 salmon_ests <- salmon_fit$NumReads
+
+stan_ests <-stan_ests * sum(salmon_ests)
 
 cor.test(salmon_ests, stan_ests, method="spearman")
 
--- read_and_store_eq_files_p2.7.py
+++ read_and_store_eq_files_p2.7.py
@@ -1,4 +1,11 @@
-eq_classes_file = open("/mnt/scratch2/mohsen/stan-test/parse_eq_classes_file/QuantificationModels/eq_classes_small.txt", "r")
+import pandas
+
+eq_classes_file = open("/mnt/scratch2/mohsen/stan-test/parse_eq_classes_file/QuantificationModels/eq_classes.txt", "r")
+
+#quant_file = pandas.read_table("/mnt/scratch2/mohsen/stan-test/salmon-0.11.3-linux_x86_64/sample_data/salmon-output/quant.sf",sep="\t")
+quant_file = pandas.read_table("/mnt/scratch2/mohsen/stan-test/salmon-output/quant.sf",sep="\t")
+
+effLens = quant_file['EffectiveLength']
 
 txps_count = int(eq_classes_file.readline())
 
@@ -15,16 +22,24 @@ counts = list()
 
 row_starts += [1]
 for i in range(eq_classes_count):
-    eq_class_info = eq_classes_file.readline().split()
-    class_size = int(eq_class_info[0])
-    for j in range(class_size):
-        columns += [int(eq_class_info[j+1])+1]
-        weights += [float(eq_class_info[j+class_size+1])]
-    row_starts += [int(row_starts[i])+class_size]        
-    counts += [int(eq_class_info[-1])]
-
+	eq_class_info = eq_classes_file.readline().split()
+	class_size = int(eq_class_info[0])
+	class_weights = list()
+	wsum = 0.0
+	for j in range(class_size):
+		columns += [int(eq_class_info[j+1])+1]
+		effLenProb = 1/effLens[int(eq_class_info[j+1])]
+		class_weights += [float(eq_class_info[j+class_size+1])*effLenProb]
+		wsum += class_weights[j]
+		weights += [float(eq_class_info[j+class_size+1])]
+	wnorm = 1.0/wsum
+	for q in range(len(class_weights)):
+		new_weight = class_weights[q]*wnorm
+		#weights += [float(new_weight)]
+	row_starts += [int(row_starts[i])+class_size]
+	counts += [int(eq_class_info[-1])]
 
 stan_data = { 'weights' : weights, 'columns' : columns, 'row_starts' : row_starts, 'counts' : counts, 'N' : len(counts), 'Q' : len(weights), 'M' : txps_count, 'alpha' : [1 for i in range(txps_count)]}
 
 import pickle
-pickle.dump( stan_data, open( "stan_data_small.p", "wb" ) )
+pickle.dump( stan_data, open( "stan_data_large.p", "wb" ) )
